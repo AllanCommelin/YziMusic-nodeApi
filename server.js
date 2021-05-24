b083d4b59ce61851179a126ceed539af4084b2c5
Allan Commelin <commelinallan@gmail.com>
2021-05-24 18:53:05 +0200

    Fix cookie on heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express'); //=> https://www.npmjs.com/package/express
 const bodyParser = require('body-parser'); //=> https://www.npmjs.com/package/body-parser
 const cookieParser = require('cookie-parser'); //=> https://www.npmjs.com/package/cookie-parser
 const passport = require('passport'); //=> https://www.npmjs.com/package/passport
-//const session = require('express-session');
+const session = require('express-session');
 //const path = require('path'); //=> https://www.npmjs.com/package/path
 // Services
 const MONGOclass = require('./services/mongo.class');
@@ -35,13 +35,14 @@ class ServerClass{
 
         //=> Use CookieParser to setup server side cookies
         this.server.use(cookieParser(process.env.COOKIE_SECRET));
-        this.server.set('trust proxy', 1)
-/*        this.server.use(session({
+        this.server.enable('trust proxy');
+        this.server.use(session({
+            resave: true,
             proxy : true,
             cookie : {
                 secure : true,
             }
-        }));*/
+        }));
 
         // Start server configuration
         this.config();
